1c2b9ef154dc51813363aa91b7b6f3bb2e70b5f1
yves-cario <yv.cario@gmail.com>
2016-10-06 14:27:16 +0200

    delete mock add http + search

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 
 import { routing } from './app.routing';
 import { AppComponent }   from './app.component';
@@ -16,7 +20,8 @@ import { DashboardComponent }  from './dashboard.component';
   [BrowserModule, 
   FormsModule, 
   routing,
-  HttpModule],
+  HttpModule,
+  InMemoryWebApiModule.forRoot(InMemoryDataService),],
   declarations: [
   AppComponent,
   DashboardComponent,
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -37,4 +37,16 @@ export class HeroesComponent implements OnInit {
     gotoDetail(): void {
         this.router.navigate(['/detail', this.selectedHero.id]);
     }
+
+    add(name: String): void {
+        name = name.trim();
+        if(!name){ return;}
+        this.heroService.create(name).then(hero => {this.heroes.push(hero);this.selectedHero = null;})
+    }
+
+    delete(hero: Hero): void {
+        this.heroService.delete(hero.id)
+            .then(()=> {this.heroes.filter(h => h !== hero);
+        if(this.selectedHero === hero){this.selectedHero =null;}});
+    }
 }
